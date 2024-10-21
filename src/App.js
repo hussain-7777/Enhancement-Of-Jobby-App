3f29e76969ea417e2f85d99f105ebc534b046e95
Mohammad Hussain <hussainmeuniq@outlook.com>
2024-10-21 04:01:31 +0000

    JobbyApp

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ import './App.css'
 const App = () => (
   <>
     <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <ProtectedRoute exact path="/" component={Home} />
-      <ProtectedRoute exact path="/jobs" component={Jobs} />
-      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-      <Route path="/not-found" component={NotFound} />
-      <Redirect to="not-found" />
+      <Route exact path='/login' component={LoginForm} />
+      <ProtectedRoute exact path='/' component={Home} />
+      <ProtectedRoute exact path='/jobs' component={Jobs} />
+      <ProtectedRoute exact path='/jobs/:id' component={JobItemDetails} />
+      <Route path='/not-found' component={NotFound} />
+      <Redirect to='not-found' />
     </Switch>
   </>
 )
